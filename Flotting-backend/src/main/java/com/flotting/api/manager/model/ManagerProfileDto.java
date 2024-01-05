a74ff70e98c4415175763db5d1f2f1179b3e7664
jangsusi <jsb568@naver.com>
2024-01-05 19:23:23 +0900

    Feat : manager CRUD 추가
    manager 프로필 승인 api추가

---
+++ Flotting-backend/src/main/java/com/flotting/api/manager/model/ManagerProfileDto.java
@@ -0,0 +1,16 @@
+package com.flotting.api.manager.model;
+
+import com.flotting.domain.ManagerProfile;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class ManagerProfileDto {
+
+    public ManagerProfileDto(ManagerProfile manager) {
+
+    }
+}
