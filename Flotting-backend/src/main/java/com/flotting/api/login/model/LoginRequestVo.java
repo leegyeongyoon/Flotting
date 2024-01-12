0aff96bf543a35039dd399f2f088876b15a6b853
igyeong-yun <xxxlsu@naver.com>
2024-01-12 10:22:32 +0900

    feat : user 로그인 테스트 코드 작성

--- Flotting-backend/src/main/java/com/flotting/api/login/model/LoginRequestVo.java
+++ Flotting-backend/src/main/java/com/flotting/api/login/model/LoginRequestVo.java
@@ -1,8 +1,10 @@
 package com.flotting.api.login.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class LoginRequestVo {
     private String phoneNumber;
     private String password;
