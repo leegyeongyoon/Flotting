a99114afdd9cdf239ba54e215710b2c489a27948
igyeong-yun <xxxlsu@naver.com>
2024-01-24 18:29:37 +0900

    feat : 로그인 기능 개발 refactor : 패키지 구조 변경

---
+++ Flotting-webapp/src/components/layout/MainNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-bottom-navigation app grow class="bg-teal-lighten-4 rounded-10">
+        <v-btn value="recent">
+            <v-icon>mdi-24px mdi mdi-account-multiple</v-icon>
+        </v-btn>
+
+        <v-btn value="favorites">
+            <v-icon>mdi-24px mdi-account-multiple-plus</v-icon>
+            <span>매니저 추가 소개</span>
+        </v-btn>
+
+        <v-btn value="nearby">
+            <v-icon>mdi-24px mdi-account-box</v-icon>
+            <span class="text-10">프로필</span>
+        </v-btn>
+    </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+    name: "MainNavigation"
+};
+</script>
+
+<style scoped></style>
