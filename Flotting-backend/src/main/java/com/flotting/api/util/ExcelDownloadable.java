7165e10357668d964c378a23d5b6894708827b2f
jangsusi <jsb568@naver.com>
2024-02-17 22:51:53 +0900

    Feat : 액셀 다운로드 기능 개
    액셀 다운로드 라이브러리 import, 액셀 데이터 생성용ExcelDownloadable 마커 인터페이스 추기

---
+++ Flotting-backend/src/main/java/com/flotting/api/util/ExcelDownloadable.java
@@ -0,0 +1,8 @@
+package com.flotting.api.util;
+
+public interface ExcelDownloadable {
+
+     String[] getHeaders();
+
+     String[] getCellDatas();
+}
---
+++ Flotting-backend/src/main/java/com/flotting/api/util/service/ExcelService.java
@@ -0,0 +1,65 @@
+package com.flotting.api.util.service;
+
+import com.flotting.api.util.ExcelDownloadable;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ExcelService {
+
+    public void downloadExcel(List<? extends Object> data, HttpServletResponse response)  {
+        Workbook workbook = new XSSFWorkbook();
+        Sheet sheet = workbook.createSheet();
+
+        String fileName = "결과 다운로드";
+        ExcelDownloadable firstExcelData = (ExcelDownloadable) data.getFirst();
+        String[] headers = firstExcelData.getHeaders();
+        Row row = sheet.createRow(0);
+        for(int i = 0 ;i< headers.length ;i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(headers[i]);
+        }
+
+        for(int i = 0; i<= data.size(); i++) {
+            ExcelDownloadable excelData = (ExcelDownloadable) data.get(i);
+            row = sheet.createRow(i + 1);
+
+            String[] cellDatas = excelData.getCellDatas();
+
+            Cell cell = null;
+            for(int j = 0; j<= cellDatas.length; j++) {
+                cell = row.createCell(i);
+                cell.setCellValue(cellDatas[j]);
+            }
+
+        }
+
+        response.setContentType("application/vnd.ms-excel");
+        try {
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8") + ".xlsx");
+        } catch (UnsupportedEncodingException e) {
+            log.error("액셀 파일 이름 인코딩 실패!");
+            throw new RuntimeException(e);
+        }
+
+        try {
+            workbook.write(response.getOutputStream());
+            workbook.close();
+        } catch (IOException e) {
+            log.error("액셀 다운로드 실패!");
+            throw new RuntimeException(e);
+        }
+    }
+}
