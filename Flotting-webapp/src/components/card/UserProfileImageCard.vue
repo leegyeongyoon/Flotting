06deeeade1f1c8390b78052162c17c3e2cc3c2a0
sunghun <sunghun5620@gmail.com>
2024-03-22 00:02:29 +0900

    feat : 플러팅 현황 레이아웃 개발

--- Flotting-webapp/src/components/card/UserProfileImageCard.vue
+++ Flotting-webapp/src/components/card/UserProfileImageCard.vue
@@ -22,17 +22,20 @@ const testUserImageUrl = ref(
 <template>
     <v-card width="100%" style="box-shadow: 4px 4px 10px 0px #60E0E0; border-radius: 16px;" :href="href">
         <div class="w-100 pa-4 d-flex flex-column ga-4 justify-center align-center">
-            <div style="width: 98px;">
-                <profile-image src="https://image.kr.canon/pds/gallery/open/1257249062209_0d8F3Kb90P.jpg" is-radius />
-            </div>
-
+            <v-card style="border-radius: 16px; width: 98px;">
+                <div
+                    class="w-100 d-flex flex-column justify-end align-start pl-6 pb-3"
+                    style="aspect-ratio: 1; background-color: #F1F1F1; background-size: cover;  background-position: 50%; background-image: url('https://image.kr.canon/pds/gallery/open/1257249062209_0d8F3Kb90P.jpg')"
+                ></div>
+            </v-card>
             <div class="text-center w-100" style="height: 63px; background: #F1F1F1; border-radius: 16px;"></div>
-            <profile-image
-                is-radius
-                title="원빈"
-                sub-title="가수, 서울 강남구"
-                src="https://i.namu.wiki/i/RmPNdn0sjKx-SYRoeRffcS5pc1rZXK-uA6PQqqU_kLFrqdMVdD3vM9HeF4c_1DLloZ1qu8KZLDwRwO8y-3vNriadzKfcXOeWaQFwwLC2Bn_HxoX1E-clw78tDqSLGLb37UHelCwkxGLeqKIpHvqPtA.webp"
-            />
+            <div
+                class="w-100 d-flex flex-column justify-end align-start pl-6 pb-3 font-weight-bold"
+                style="border-radius: 16px; aspect-ratio: 1; background-color: #F1F1F1; background-size: cover;  background-position: 50%; background-image: url('https://i.namu.wiki/i/RmPNdn0sjKx-SYRoeRffcS5pc1rZXK-uA6PQqqU_kLFrqdMVdD3vM9HeF4c_1DLloZ1qu8KZLDwRwO8y-3vNriadzKfcXOeWaQFwwLC2Bn_HxoX1E-clw78tDqSLGLb37UHelCwkxGLeqKIpHvqPtA.webp'); font-size: 18px; text-shadow: -1px 0px white, 0px 1px white, 1px 0px white, 0px -1px white; "
+            >
+                <span>원빈</span>
+                <span>가수, 서울 강남구</span>
+            </div>
         </div>
     </v-card>
 </template>
--- Flotting-webapp/src/components/layout/MainNavigation.vue
+++ Flotting-webapp/src/components/layout/MainNavigation.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="position-fixed w-100" style="bottom: 0; height: 80px; background-color: #60e0e0; z-index: 1000;">
         <div class="d-flex justify-space-between align-center mx-auto h-100 ga-0" style="max-width: 1024px; min-width: 360px;">
-            <v-btn height="100%" style="width: calc(50% - 1px); border-radius: 0; box-shadow: none;" color="#60e0e0" href="/plotting">
+            <v-btn height="100%" style="width: calc(50% - 1px); border-radius: 0; box-shadow: none;" color="#60e0e0" href="/dashboard">
                 <div class="d-flex flex-column justify-center align-center" style="gap: 4px;">
                     <account-icon width="39" height="39" />
                     <span class="text-white font-weight-bold" style="font-size: 14px; letter-spacing: 0;">이번주 프로필</span>
                 </div>
             </v-btn>
             <div style="width: 2px; height: 100%; background-color: white;"></div>
-            <v-btn height="100%" style="width: calc(50% - 1px); border-radius: 0; box-shadow: none;" color="#60e0e0" href="/setting">
+            <v-btn height="100%" style="width: calc(50% - 1px); border-radius: 0; box-shadow: none;" color="#60e0e0" href="/plotting">
                 <div class="d-flex flex-column justify-center align-center" style="gap: 4px;">
                     <heart-icon></heart-icon>
                     <span class="text-white font-weight-bold" style="font-size: 14px; letter-spacing: 0;">플러팅 현황</span>
@@ -20,6 +20,7 @@
 
 <script setup>
 import HeartIcon from "@/components/icon/HeartIcon.vue";
+
 import AccountIcon from "@/components/icon/AccountIcon.vue";
 </script>
 
